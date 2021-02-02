89f671b2f830134194d161c38660f7acf65bba13
thelvir <77773439+thelvir@users.noreply.github.com>
2021-02-02 18:43:50 +0400

    Create 0001 - Simple problem - Простая задача - Sadə məsələ.c

---
+++ 0000-0999/0001 - Simple problem - Простая задача - Sadə məsələ.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+
+    int n,a,b;
+
+    scanf("%d",&n );
+    a=n/10;
+    b=n%10;
+    
+    printf("%d %d",a,b );
+    
+	return 0;
+}
