dd3843410f6e6e12f24bdb0bacae227643e1f278
thelvir <77773439+thelvir@users.noreply.github.com>
2021-02-04 12:57:19 +0400

    Create 0500 - Repair - Ремонт.c

---
+++ 0000-0999/0500 - Repair - Ремонт.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	long a ,b ,x ,y, c , m , n, l;
+	
+	scanf("%d",&y );
+	for(y=y ; y>0 ; y-=1){
+	
+	scanf("%ld" "%ld" "%ld",&a,&b,&c );
+	x = (2*(a+b))*c;
+	printf("%ld\n", (x+15)/16);
+}
+	
+	return 0;
+}
