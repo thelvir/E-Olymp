503e9676b10010050a78f54ee906d380bb11aba4
thelvir <77773439+thelvir@users.noreply.github.com>
2021-02-04 12:43:34 +0400

    Create 0248 - Young gardener - Юный садовод - Gənc bağban.c

---
+++ 0000-0999/0248 - Young gardener - Юный садовод - Gənc bağban.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+ {
+    int n,s;
+    scanf("%d",&n);
+    s=n*(n+1)+1;
+    printf("%d",s);
+    
+	return 0;
+}
