9424260256baeaf8d6d9b4d230799a6169a5c70d
thelvir <77773439+thelvir@users.noreply.github.com>
2021-02-05 15:46:37 +0400

    Create 0519 - Sum of squares - Сумма квадратов.c

---
+++ 0000-0999/0519 - Sum of squares - Сумма квадратов.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	long a,b,c;
+	scanf("%ld %ld",&a,&b);
+	c=a*a+b*b;
+	a!=b;
+	printf("%ld",c);
+	return 0;
+}
