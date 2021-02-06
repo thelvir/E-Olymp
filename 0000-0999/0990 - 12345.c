7013169d0bf508fbe05e76beb497225b364ddd5b
thelvir <77773439+thelvir@users.noreply.github.com>
2021-02-06 10:02:43 +0400

    Create 0990 - 12345.c

---
+++ 0000-0999/0990 - 12345.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	printf("1\n");
+	printf("2\n");
+	printf("3\n");
+        printf("4\n");
+        printf("5\n");
+	return 0;
+}
